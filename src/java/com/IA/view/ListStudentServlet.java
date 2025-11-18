6c090050376e397120273b8c557a48e9528342d4
Kyran-Git <niklauscareer@gmail.com>
2025-11-18 23:00:08 +0800

    Renamed the ListStudent servlet into ListStudentServlet

--- src/java/com/IA/view/ListStudentServlet.java
+++ src/java/com/IA/view/ListStudentServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author nikla
  */
-public class ListStudent extends HttpServlet {
+public class ListStudentServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
